bec50a2a2f72291245e96efcc9c34aef356631d1
Nayana Davis <nayana487@gmail.com>
2015-07-27 22:29:35 -0400

    intro to rails homework

---
+++ WDInstagram/app/controllers/entries_controller.rb
@@ -0,0 +1,40 @@
+class EntriesController < ApplicationController
+  def index
+    @entries = Entry.all
+  end
+
+  def new
+   @entry = Entry.new
+  end
+
+  def create
+    @entry = Entry.new( params[:entry] )
+    @entry.update( date_taken: Time.now.strftime("%B %d, %Y") )
+    if @entry.save
+      redirect_to "/entries/#{@entry.id}"
+    else
+      redirect_to "/entries/new"
+    end
+ end
+
+ def edit
+   @entry = Entry.find( params[:id] )
+ end
+
+ def update
+   @entry = Entry.find( params[:id] )
+   @entry.update( params[:entry] )
+   redirect_to "/entries/#{@entry.id}"
+ end
+
+ def show
+   @entry = Entry.find( params[:id] )
+ end
+
+ def delete
+   @entry = Entry.find( params[:id] )
+   @entry.destroy
+   redirect_to "/"
+ end
+ 
+end
---
+++ readme.md
@@ -0,0 +1,7 @@
+# WDInstagram
+## Port the app!
+- You can find a solution to WDInstagram [here](https://github.com/ga-dc/wdinstagram/tree/solution)
+- Create a new rails application and port the code inside the [sinatra version above](https://github.com/ga-dc/wdinstagram/tree/solution)
+- Create a new repo on github for this application.
+- Submit an issue to this repo with the link to your repo with the rails app in it.
+- Along with your link, submit user stories for the Blog application you will be creating for hw throughout the week.
